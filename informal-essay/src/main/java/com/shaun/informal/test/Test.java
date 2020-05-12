b1c38d922d87cbef9d39e1fa8deea4af2cefc3ed
shadengyao <shaun_sha@foxmail.com>
2020-05-12 15:15:06 +0800

    test

---
+++ informal-essay/src/main/java/com/shaun/informal/test/Test.java
@@ -0,0 +1,9 @@
+package com.shaun.informal.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new String[0].getClass());
+        System.out.println(System.getProperty("os.name"));
+    }
+}
