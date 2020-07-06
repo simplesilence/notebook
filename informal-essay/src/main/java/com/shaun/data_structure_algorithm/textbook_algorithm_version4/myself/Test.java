7789098c05cc757973cca46284db2e0e832fe625
simplesilence <1317495534@qq.com>
2020-07-06 10:03:26 +0800

    delete official package

---
+++ informal-essay/src/main/java/com/shaun/data_structure_algorithm/textbook_algorithm_version4/myself/Test.java
@@ -0,0 +1,27 @@
+package com.shaun.data_structure_algorithm.textbook_algorithm_version4.myself;
+
+/**
+ * 常用小算法
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(test1("   123321"));
+    }
+
+    /**
+     * 判断给定的字符串是否是回文
+     * @param str 给定的字符串
+     * @return
+     */
+    public static boolean test1(String str){
+
+        if(null != str && "".equals(str = str.trim())){
+
+        }
+
+        System.out.println(str);
+
+        return false;
+    }
+}
